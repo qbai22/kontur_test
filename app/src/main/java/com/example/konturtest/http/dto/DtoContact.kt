cea95fbddf47c189a0706c9a4ef6310ece9b585d
Vova <awatarmumu0@gmail.com>
2019-10-24 13:14:06 +0500

    - added dto objects

---
+++ app/src/main/java/com/example/konturtest/http/dto/DtoContact.kt
@@ -0,0 +1,27 @@
+package com.example.konturtest.http.dto
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by Vladimir Kraev
+ */
+data class DtoContact(
+
+    var id: String,
+
+    var name: String? = null,
+
+    var phone: String? = null,
+
+    var height: Float = 0F,
+
+    @SerializedName("biography")
+    var bio: String? = null,
+
+    @SerializedName("temperament")
+    var temper: String? = null,
+
+    @SerializedName("educationPeriod")
+    var edPeriod: EducationPeriod
+
+)
---
+++ app/src/main/java/com/example/konturtest/http/dto/EducationPeriod.kt
@@ -0,0 +1,12 @@
+package com.example.konturtest.http.dto
+
+/**
+ * Created by Vladimir Kraev
+ */
+data class EducationPeriod(
+
+    var start: String? = null,
+
+    var end: String? = null
+
+)
